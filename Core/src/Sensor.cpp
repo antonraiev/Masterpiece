f22f8304ea12348e0f1baa5e01c241b8122e087b
Anton Rayev <sfdsrc@gmail.com>
2013-10-20 13:09:08 +0300

    Implement Sensor class
    Stubs for the sensor's value since the real sensor is not available

--- Core/src/Sensor.cpp
+++ Core/src/Sensor.cpp
@@ -1,32 +1,51 @@
 #include "Sensor.h"
 
+#include <cstdlib>
+#include <ctime>
+
 namespace Core 
 {
-    Sensor::Sensor(SensorType type, double lowerBound, double upperBound, double measurementError)
+    Sensor::Sensor(SensorType type, double lowerBound, double upperBound, double measurementError) :
+        type(type),
+        lowerBound(lowerBound),
+        upperBound(upperBound),
+        measurementError(measurementError)
     {
+        this->update();
+        // initial value (stub)
+        value = 25;
     }
 
     double Sensor::getLowerBound() const
     {
+        return lowerBound;
     }
 
     double Sensor::getValue() const
     {
+        return value;
     }
 
     double Sensor::getUpperBound() const
     {
+        return upperBound;
     }
 
     SensorType Sensor::getType() const
     {
+        return type;
     }
 
     double Sensor::getMeasurementError() const
     {
+        return measurementError;
     }
 
     void Sensor::update()
     {
+        // stub for now
+        srand(time(NULL));
+        int sign = (rand() % 100) > 80 ? -1 : 1;
+        value += sign * (static_cast<double>(rand() % 4) / 10.f);
     }
 } // namespace Core
