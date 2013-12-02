8d5766cfc2649f2741d825a0dae8ba84a8fb7d33
Anton Rayev <sfdsrc@gmail.com>
2013-12-02 02:47:07 +0200

    Implement EffectorMemory

--- Core/include/EffectorMemoryImpl.h
+++ Core/include/EffectorMemoryImpl.h
@@ -2,14 +2,20 @@
 #define EFFECTORMEMORYIMPL_H
 
 #include "Granule.h"
-#include "IFuzzyController.h"
+#include "IControlMediator.h"
+#include "FuzzyOperations.h"
+
+#include <map>
+#include <cassert>
 
 namespace Core 
 {
     /** 
     * Computes and holds the resulting control signals using rule activities
+    * @tparam TSensorType Type of sensor enum
+    * @tparam TControlType Type of control enum
     */
-    template <typename TControlType>
+    template <typename TSensorType, typename TControlType>
     class EffectorMemory
     {
     public:
@@ -17,13 +23,13 @@ namespace Core
         * Create effector memory
         * @param fuzzyController Reference to mediator
         */
-        explicit EffectorMemory(IFuzzyController<TControlType> *fuzzyController)
+        explicit EffectorMemory(IControlMediator<TSensorType,
+            TControlType> *controlMediator) :
+            controlMediator(controlMediator)
         {
-            this->fuzzyController = fuzzyController;
-
             for(TControlType type = TControlType::First; 
                 type != TControlType::Last; ++type) {
-                    control[type] = Granule(0, 0, 0);
+                    control[type] = Granule();
             }
         }
 
@@ -31,9 +37,9 @@ namespace Core
         * Get the control signal for specified control type
         * @param type Type of control
         */
-        Granule& getControl(TControlType type) const
+        const Granule& getControl(TControlType type) const
         {
-            return Granule(0, 0, 0);
+            return control.find(type)->second;
         }
 
         /** 
@@ -41,11 +47,49 @@ namespace Core
         */
         void update()
         {
+            assert(controlMediator && "Mediator is null");
+
+            for(auto &controlPair : control) {
+
+                // Get rule activity from prototype memory for each rule
+                std::vector<std::pair<Granule, Granule>> ruleActivities 
+                    = controlMediator->getRuleActivity(controlPair.first);
+                assert(ruleActivities.size());
+
+                // Get initial alpha and beta from first rule
+                const std::pair<Granule, Granule> &firstRuleActivity 
+                    = *ruleActivities.begin();
+                const Granule &firstActivity = firstRuleActivity.first;
+                const Granule &firstControl = firstRuleActivity.second;
+
+                double ovarallAlpha = Fuzzy::operationG(firstActivity.alpha,
+                    firstControl.alpha, firstActivity.beta, firstControl.beta);
+                double overallBeta = std::max(firstControl.beta,
+                    firstActivity.beta);
+
+                for(auto ruleActivity = ++ruleActivities.begin();
+                    ruleActivity != ruleActivities.end(); ++ruleActivity) {
+
+                    const Granule &activity = ruleActivity->first;
+                    const Granule &control = ruleActivity->second;
+
+                    double gAlpha = Fuzzy::operationG(activity.alpha,
+                        control.alpha, activity.beta, control.beta);
+
+                    // Accumulate alpha and beta with other rule activities
+                    ovarallAlpha = Fuzzy::alphaAccumulation(ovarallAlpha,
+                        gAlpha);
+                    overallBeta = std::max(overallBeta, std::max(control.beta,
+                        activity.beta));
+                }
+                controlPair.second.alpha = ovarallAlpha;
+                controlPair.second.beta = overallBeta;
+            }
         }
 
     private:
         std::map<TControlType, Granule> control;
-        IFuzzyController<TControlType> *fuzzyController;
+        IControlMediator<TSensorType, TControlType> *controlMediator;
     };
 } // namespace Core
 
