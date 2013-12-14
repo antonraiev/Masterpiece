afe116d6a2add158216dd74d6eb3cf8b8899a623
Anton Rayev <sfdsrc@gmail.com>
2013-12-14 17:38:43 +0200

    Remove template parameter from Actuator class

--- Core/include/Actuator.h
+++ Core/include/Actuator.h
@@ -1,1 +1,38 @@
-#include "ActuatorImpl.h"
+#ifndef ACTUATOR_H
+#define ACTUATOR_H
+
+namespace Core
+{
+    enum class ActuatorState {REST, HEATING, ACTIVE};
+
+    class Actuator
+    {
+    public:
+        Actuator(size_t startDelay, size_t stopDelay);
+
+        void setStartDelay(size_t startDelay);
+        void setStopDelay(size_t stopDelay);
+        size_t getStartDelay() const;
+        size_t getStopDelay() const;
+        size_t getHeatingDelay() const;
+        size_t getActiveDelay() const;
+        ActuatorState getState() const;
+        bool getControl() const;
+        void update(double alpha);
+
+    private:
+        void processRestState(double alpha);
+        void processHeatingState(double alpha);
+        void processActiveState(double alpha);
+
+    private:
+        ActuatorState state;
+        size_t startDelay;
+        size_t stopDelay;
+        
+        size_t heatingDelay;
+        size_t activeDelay;
+    };
+}
+
+#endif
--- Core/src/Actuator.cpp
+++ Core/src/Actuator.cpp
@@ -1,1 +1,101 @@
 #include "Actuator.h"
+
+namespace Core
+{
+    Actuator::Actuator(size_t startDelay, size_t stopDelay) :
+        startDelay(startDelay),
+        stopDelay(stopDelay)
+    {
+        state = ActuatorState::REST;
+    }
+
+    void Actuator::setStartDelay(size_t startDelay)
+    {
+        this->startDelay = startDelay;
+    }
+
+    void Actuator::setStopDelay(size_t stopDelay)
+    {
+        this->stopDelay = stopDelay;
+    }
+
+    size_t Actuator::getStartDelay() const
+    {
+        return startDelay;
+    }
+
+    size_t Actuator::getStopDelay() const
+    {
+        return stopDelay;
+    }
+
+    size_t Actuator::getHeatingDelay() const
+    {
+        return heatingDelay;
+    }
+
+    size_t Actuator::getActiveDelay() const
+    {
+        return activeDelay;
+    }
+
+    ActuatorState Actuator::getState() const
+    {
+        return state;
+    }
+
+    bool Actuator::getControl() const
+    {
+        if(state == ActuatorState::ACTIVE) {
+            return true;
+        }
+        return false;
+    }
+
+    void Actuator::update(double alpha)
+    {
+        switch(state)
+        {
+        case ActuatorState::REST:
+            this->processRestState(alpha);
+            break;
+
+        case ActuatorState::HEATING:
+            this->processHeatingState(alpha);
+            break;
+                
+        case ActuatorState::ACTIVE:
+            this->processActiveState(alpha);
+            break;
+        }
+    }
+
+    void Actuator::processRestState(double alpha)
+    {
+        if(alpha > 0) {
+            heatingDelay = startDelay;
+            state = ActuatorState::HEATING;
+        }
+    }
+
+    void Actuator::processHeatingState(double alpha)
+    {
+        --heatingDelay;
+        if(heatingDelay == 0) {
+            activeDelay = stopDelay;
+            state = ActuatorState::ACTIVE;
+        }
+    }
+
+    void Actuator::processActiveState(double alpha)
+    {
+        if(alpha > 0) {
+            activeDelay = stopDelay;
+        } else {
+            --activeDelay;
+        }
+        if(activeDelay == 0) {
+            state = ActuatorState::REST;
+        }
+    }
+}
